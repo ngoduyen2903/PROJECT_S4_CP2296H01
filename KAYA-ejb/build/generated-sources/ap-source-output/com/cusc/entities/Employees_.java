83237b3a0058490c3621dd4303e4d1633a31b88c
hung <hthungat@gmail.com>
2023-04-03 21:21:28 +0700

    h

--- KAYA-ejb/build/generated-sources/ap-source-output/com/cusc/entities/Employees_.java
+++ KAYA-ejb/build/generated-sources/ap-source-output/com/cusc/entities/Employees_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-04-01T16:30:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-04-03T21:20:11")
 @StaticMetamodel(Employees.class)
 public class Employees_ { 
 
